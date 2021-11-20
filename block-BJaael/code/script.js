184d6a0f960aef3efc114ac8378228a7945129b9
Chandan Kumar <chandanchandu287@gmail.com>
2021-11-20 22:14:45 +0530

    Refactor code

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -4,16 +4,21 @@ body.append(div);
 
 function createBox() {
     let box = document.createElement("div");
+    let h3 = document.createElement("h3");
     box.style.height = "4rem";
     box.style.width = "4rem";
     box.style.border = "0.15rem solid black";
     box.style.background = '#' + Math.floor(Math.random() * 899) + 100;
+    h3.innerText = Math.floor(Math.random() * 500);
     div.append(box);
+    box.append(h3);
 }
 
 function createDisco() {
-    for (i = 0; i < 190; i++) {
+    for (i = 0; i < 500; i++) {
         createBox();
     }
+    
 }
-setInterval(createDisco, );
+createDisco();
+div.addEventListener("mousemove", createBox)
